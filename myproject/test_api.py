5375673edd03b1dfde5ea0766aaad9b53512462f
VanBeemenN <r0882111@student.thomasmore.be>
2023-12-30 18:38:21 +0100

    testing

--- myproject/test_api.py
+++ myproject/test_api.py
@@ -1,71 +1,48 @@
-from datetime import date
-from fastapi.testclient import TestClient
+import requests
 
-from myproject.main import app
-from myproject.models import Festival
-
-client = TestClient(app)
-
-# Define testing headers with token
-headers = {
-    "accept": "application/json",
-    "Content-Type": "application/json"
-}
+base_url = "http://127.0.0.1:8000/"
 
 def test_create_festival():
-    """Test creating a new festival."""
     festival_data = {
         "naam": "Test Festival",
         "locatie": "Test Location",
-        "start_datum": str(date.today()),
-        "eind_datum": str(date.today()),
+        "start_datum": "2023-01-01",
+        "eind_datum": "2023-01-02",
         "land_id": 1
     }
+    response = requests.post(f"{base_url}/create_festival", json=festival_data)
+    assert response.status_code == 200
+    response_data = response.json()
+    assert "message" in response_data
+    assert response_data["message"] == 'Festival created successfully'
 
-    response = client.post("/festivals", json=festival_data, headers=headers)
-
-    assert response.status_code == 201
-    created_festival = response.json()
-    assert isinstance(created_festival, Festival)
-    assert created_festival.naam == festival_data["naam"]
-
-def test_get_festivals():
-    """Test getting a list of festivals."""
-    response = client.get("/festivals", headers=headers)
-
+def test_read_festivals():
+    response = requests.get(f"{base_url}/festivals")
     assert response.status_code == 200
     festivals = response.json()
     assert isinstance(festivals, list)
-    assert all(isinstance(festival, Festival) for festival in festivals)
 
-def test_get_festival():
-    """Test getting a specific festival by ID."""
-    response = client.get("/festivals/1", headers=headers)
-
+def test_read_festival():
+    # Assuming there is at least one festival in the database
+    response = requests.get(f"{base_url}/festivals/1")
     assert response.status_code == 200
     festival = response.json()
-    assert isinstance(festival, Festival)
+    assert isinstance(festival, dict)
 
 def test_update_festival():
-    """Test updating an existing festival."""
     festival_data = {
         "naam": "Updated Festival",
         "locatie": "Updated Location",
-        "start_datum": str(date.today()),
-        "eind_datum": str(date.today()),
+        "start_datum": "2023-01-01",
+        "eind_datum": "2023-01-02",
         "land_id": 1
     }
-
-    response = client.put("/festivals/1", json=festival_data, headers=headers)
-
+    response = requests.put(f"{base_url}/festivals/1", json=festival_data)
     assert response.status_code == 200
     updated_festival = response.json()
-    assert isinstance(updated_festival, Festival)
-    assert updated_festival.naam == festival_data["naam"]
+    assert updated_festival["naam"] == festival_data["naam"]
 
 def test_delete_festival():
-    """Test deleting a festival by ID."""
-    response = client.delete("/festivals/1", headers=headers)
-
+    response = requests.delete(f"{base_url}/festivals/1")
     assert response.status_code == 200
     assert response.json() == {"message": "Festival deleted successfully"}
